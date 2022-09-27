e3cfdde6539289cfac3946d119eeca0e58030119
Anvarjon <114461798+anvarjondev@users.noreply.github.com>
2022-09-27 11:45:09 +0500

    Yangi fayllar qo'shildi
    6-7-8-9-10-11 darslar yuklandi

---
+++ dars-06.py
@@ -0,0 +1,68 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+#son = int(input("Istagan son kirit\n"))
+#son_kv = son**2
+#son_kub = son**3
+#print(str(son) + " ning kvadrati " + str(son_kv) + " ga teng")
+#print(str(son) + " ning kubi " + str(son_kub) + " ga teng")
+
+#a = 28
+#b = -20
+#c = a + b
+#print(c)
+
+#pi=3.14159
+#radius = 10
+#diametr = 2 * radius
+#print("Aylana uzunligi ", pi*diametr, " ga teng")
+#a= -20
+
+#a= 40
+#c = b/a
+#print(c)
+
+#a = 2 
+#b = 3.0
+#print(a+b)
+#print(a*b)
+#print(a**b)
+#print(a*(a+b))
+
+#axoli_soni = 7_594_235_000
+#print("Yer yuzsida hozirda ", axoli_soni, " odam bor")
+
+#PI = 3.14159
+#radius = 21.2
+
+#x, y, z = 10, 11, 20
+
+#ism = "Jobir"
+#yosh = 20
+#xabar = ism + ' ' + str(yosh) + ' yoshda'
+#print(xabar)
+
+#fodalanuvchilarni tug'ilgan yoshini hisoblash
+#t_yil = int(input("Tug'ilgan yilingizni kiriting: "))
+#yosh = 2022 - t_yil
+#print("Siz " + str(yosh) + " yoshda ekansiz")              
+
+#yosh = int(input("Yoshingiz nechida :  "))
+#yil = 2022 - yosh
+#print("Siz " + str(yil) + " da tug'ilgansiz")
+
+#a = float(input("Birinchi sonni kiriting: "))
+#b = float(input("Ikkinchi sonni kiriting: "))
+#print(str(a) , " + " , str(b) + " = " , str(a + b))
+#print(str(a), " - " , str(b) , " = ", str(a - b))
+#print(str(a), " * " , str(b) , " = ", str(a * b))
+#print(str(a), " / " , str(b) , " = ", str(a / b))
+
+
+
+
+
+
