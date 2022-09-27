0da2d17231b8b4f0cce12aea7ee4ade85880cfb2
Anvarjon <114461798+anvarjondev@users.noreply.github.com>
2022-09-27 12:20:35 +0500

    Yangi fayllar qo'shildi
    3-4 darslar yuklandi

---
+++ dars-3.py
@@ -0,0 +1,14 @@
+#print("\"Nexia\", \"Tico\", 'Damas' ko'rganlar qilar havas")
+#5 ning 4-darajasini toping
+#print(5**4)
+#22ni 4ga bo'lganda qancha qoldiq qoladi?
+#print(22%4)
+#Tomonlari 125ga teng kvadratning yuzi va perimetrini toping?
+#print("Kvadratni tomoni 125ga teng uning yuzi S=", 125**2 ," va kvadratni perimetri P=", 125*4) 
+#Diametri 12 ga teng bo'lgan doiaring yuzini toping.(π=3.14 de oling)
+#print("""Diametri 12 ga teng bo'lgan doirani yuzini toping?
+#Doirani yuzini formulasi S=πr² bo'lsa, diametri d= 12 bundan
+ #     r=d/2 =""", 12/2, 'S =', 3.14*6**2)
+ #Katetlari 6 va 7 bo'lgan to'g'ri burchakli uchburchakli uchburchakning
+ #gipotenuzasini toping(Pifogor teoremasidan foydalaning)
+print("Katetlari 6 va 7 bo'lgan to'g'ri burchakli uchburchakning gipotenuzasi", (6**2+7**2)**(1/2))
---
+++ dars-4.py
@@ -0,0 +1,10 @@
+ism = "Abdulloh"
+yosh = 22
+print(ism)
+print(yosh)
+ism = "Muhammad"
+print(ism)
+a = 6
+b = 7 
+c = (a + b)**2
+print(c)
